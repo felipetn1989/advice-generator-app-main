9ab24e482609982d9b1708c838dde99950d185ba
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-03-13 15:12:23 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,16 +1,16 @@
 async function getAdvice() {
   let min = 1;
   let max = 220;
-  let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  let randomNum = Math.floor(Math.random() * (max - min + 1)) + min; //generating a random number between 1 and 220
   const response = await fetch(`https://api.adviceslip.com/advice/${117}`);
-  const adviceObj = await response.json();
+  const adviceObj = await response.json(); //fething the object containing the quote ID and the advice itself
 
   console.log(adviceObj);
 
   adviceId.innerHTML = adviceObj.slip.id;
-  adviceQuote.innerHTML = adviceObj.slip.advice;
+  adviceQuote.innerHTML = adviceObj.slip.advice; //adding the elements to the page dynamically
 }
 
-getAdvice();
+getAdvice(); 
 
-adviceBtn.addEventListener("click", getAdvice);
+adviceBtn.addEventListener("click", getAdvice); //adding the click event to the button
