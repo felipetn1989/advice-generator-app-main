172f10d40b13d3e520163105f7f952b2f64425dc
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-03-13 15:14:05 -0300

    v1 finished

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ async function getAdvice() {
   let min = 1;
   let max = 220;
   let randomNum = Math.floor(Math.random() * (max - min + 1)) + min; //generating a random number between 1 and 220
-  const response = await fetch(`https://api.adviceslip.com/advice/${117}`);
+  const response = await fetch(`https://api.adviceslip.com/advice/${randomNum}`);
   const adviceObj = await response.json(); //fething the object containing the quote ID and the advice itself
 
   console.log(adviceObj);
