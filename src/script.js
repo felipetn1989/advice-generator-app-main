ee0bda9dd95e1f2b9bf595b39228c05cee805d3c
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-03-13 15:11:20 -0300

    readme

--- src/script.js
+++ src/script.js
@@ -1,9 +1,16 @@
 async function getAdvice() {
-  const response = await fetch("https://api.adviceslip.com/advice/117");
+  let min = 1;
+  let max = 220;
+  let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  const response = await fetch(`https://api.adviceslip.com/advice/${117}`);
   const adviceObj = await response.json();
+
+  console.log(adviceObj);
 
   adviceId.innerHTML = adviceObj.slip.id;
   adviceQuote.innerHTML = adviceObj.slip.advice;
 }
 
 getAdvice();
+
+adviceBtn.addEventListener("click", getAdvice);
